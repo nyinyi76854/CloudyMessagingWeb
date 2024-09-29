bba1ed069f0be2a1ee4c1f39a6abc35204475eaa
nyinyi76854 <148785112+nyinyi76854@users.noreply.github.com>
2024-09-29 18:22:44 +0630

    Update sendingnotification.py

--- sendingnotification.py
+++ sendingnotification.py
@@ -3,9 +3,9 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import firebase_admin
 from firebase_admin import credentials, db
-
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate("serviceAccountKey.json")  # Replace with your service account path
+import os
+firebase_service_account_key = os.getenv('FIREBASE_SERVICE_ACCOUNT_KEY')
+cred = credentials.Certificate(firebase_service_account_key)  # Replace with your service account path
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://chatflow-59776-default-rtdb.firebaseio.com'
 })
